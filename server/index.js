a89baeb00700f2c2e08dcc78906d708a9df59f88
Antonio <amp.olivas00@gmail.com>
2021-10-15 12:25:53 -0600

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["Do you know that the busiest person has the largest amount of time?", "Don’t just think, act!", 
+"Don’t let your limitations overshadow your talents.", "A hunch is creativity trying to tell you something.", 
+"The best prediction of future is the past.",
+];
+
+let randomIndex = Math.floor(Math.random() * fortunes.length);
+let randomFortune = fortunes[randomIndex];
+
+ res.status(200).send(randomFortune)
+});
+
+
+app.get("/api/motivation", (req, res) => {
+  const motivationBest = ["Good, better, best. Never let it rest. 'Til your good is better and your better is best.",
+  "It always seems impossible until it's done.", "A good head and a good heart are always a formidable combination.",
+  "Celebrate Your Small Wins",
+];
+
+let randomIndex = Math.floor(Math.random() * motivationBest.length);
+let randomMotivation = motivationBest[randomIndex];
+
+ res.status(200).send(randomMotivation);
+});
+
+
+// app.get("/api/motivation", (req, res) => {
+//   res.status(301).redirect("https://www.youtube.com/watch?v=fxIlVbvOHyY")
+// });
+
+
+
+app.listen(4000, () => console.log("Server running on 4000"));
